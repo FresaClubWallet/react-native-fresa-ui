7c38ae316d2a8613f8de75dca6c946f46fc3e453
Mat Waller <wallermadev@gmail.com>
2022-03-30 12:54:06 +0100

    Updating dummy data and removing orderdelivery page

--- screens/index.js
+++ screens/index.js
@@ -1,11 +1,9 @@
 import Login from "./Login"
 import Home from "./Home"
 import Restaurant from "./Restaurant"
-import OrderDelivery from "./OrderDelivery"
 
 export {
     Login,
     Home,
     Restaurant,
-    OrderDelivery,
 }
