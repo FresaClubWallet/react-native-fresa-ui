f209a5dda51d84f2392485c4297c748580e12c86
Mat Waller <wallermadev@gmail.com>
2022-03-30 21:45:51 +0100

    Clearing up dummy data.

--- screens/Home.js
+++ screens/Home.js
@@ -20,6 +20,7 @@ import MyProductsSlider from "../components/dashboard/MyProductsSlider"
 const NETWORK = 'https://alfajores-forno.celo-testnet.org'; //test net
 const cUSD_ADDRESS = "0x874069fa1eb16d44d622f2e0ca25eea172369bc1";
 const CONTRACT_ADDRESS = "0xba2C8354c22F1C8033DF24669a6a0920869157B8";
+const products = [];
 
 const shortenAddress = (address) => {
     return `${address.slice(0, 6)}...${address.slice(
@@ -47,185 +48,48 @@ const Home = ({ navigation }) => {
         const none = await provider.getBalance(cUSD_ADDRESS)
         let q = await ethers.utils.formatEther(none)
         setBalance((+q).toFixed(2))
-        callMethod();
+        //callMethod();
     }, [balance])
 
     const callMethod = async () => {
         try {
-            const readStoreFront = await contract.readStoreFront("0xd15c1e42c589b3800119bc5bf3d627ec20fb7cd5")
+            const readStoreFront = await contract.readProduct("0x9f3DD64c084C88e8E456e9BAdbc1ebbC624941be", 0)
             console.log(readStoreFront)
         } catch (e) {
             console.error(e);
         }
     };
 
-
-    // Dummy Datas
-
-    const initialCurrentLocation = {
-        streetName: "Fresa",
-        gps: {
-            latitude: 1.5496614931250685,
-            longitude: 110.36381866919922
+    const getProduct = async (address, index) => {
+        try {
+            const readStoreFront = await contract.readProduct(address, index)
+            console.log(readStoreFront)
+        } catch (e) {
+            console.error(e);
         }
-    }
+    };
 
-    const orderData = [
-        {
-            id: 1,
-            image: "https://i.ibb.co/dJRxXmM/060fedb37f864637a7287b2e818c24a4.png",
-            address: "",
-            vendor_title: "Berry Nice Strawberries",
-            vendor_description: "Fresh Strawberries",
-            total_price: 100,
-            isVendorTx: true
-        },
-        {
-            id: 2,
-            address: "",
-            image: "https://i.ibb.co/dJRxXmM/060fedb37f864637a7287b2e818c24a4.png",
-            vendor_title: "Berry Nice Strawberries",
-            vendor_description: "Fresh Strawberries",
-            total_price: 200,
-            isVendorTx: false
-        },
-        {
-            id: 3,
-            address: "",
-            image: "https://i.ibb.co/dJRxXmM/060fedb37f864637a7287b2e818c24a4.png",
-            vendor_title: "Berry Nice Strawberries",
-            vendor_description: "Fresh Strawberries",
-            total_price: 100,
-            isVendorTx: false
-        },
-        {
-            id: 4,
-            address: "",
-            image: "https://i.ibb.co/dJRxXmM/060fedb37f864637a7287b2e818c24a4.png",
-            vendor_title: "Berry Nice Strawberries",
-            vendor_description: "Fresh Strawberries",
-            total_price: 700,
-            isVendorTx: false
-        },
-        {
-            id: 5,
-            address: "",
-            image: "https://i.ibb.co/dJRxXmM/060fedb37f864637a7287b2e818c24a4.png",
-            vendor_title: "Berry Nice Strawberries",
-            vendor_description: "Fresh Strawberries",
-            total_price: 700,
-            isVendorTx: false
-        },
-        {
-            id: 6,
-            address: "",
-            image: "https://i.ibb.co/dJRxXmM/060fedb37f864637a7287b2e818c24a4.png",
-            vendor_title: "Berry Nice Strawberries",
-            vendor_description: "Fresh Strawberries",
-            total_price: 700,
-            isVendorTx: false
-        },
-        {
-            id: 7,
-            address: "",
-            image: "https://i.ibb.co/dJRxXmM/060fedb37f864637a7287b2e818c24a4.png",
-            vendor_title: "Berry Nice Strawberries",
-            vendor_description: "Fresh Strawberries",
-            total_price: 700,
-            isVendorTx: false
-        },
-    ];
+    const getProducts = async (address) => {
+        try {
+            // Get Product count from store.
+            const productCount = await contract.readProductCount(address);
+            products.splice(0, productCount);
+            for(var i = 0;i < productCount; i++){
+                const _p = await contract.readProduct(address, i)
+                products.push(
+                    _p
+                )
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
     const categoryData = [
         {
             id: 1,
             name: "Berry Nicee Strawberries",
-            rating: 4.8,
-            categories: [5, 7],
-            priceRating: affordable,
-            photo: images.burger_restaurant_1,
-            duration: "30 - 45 min",
             image: "https://i.ibb.co/dJRxXmM/0160fedb37f864637a7287b2e818c24a4.png",
-            location: {
-                latitude: 1.5347282806345879,
-                longitude: 110.35632207358996,
-            },
-            menu: [
-                {
-                    menuId: 1,
-                    name: "1 Basket of fresh strawberries.",
-                    photo: images.strawberry_picking,
-                    description: "A Basket of fresh strawberries picked for our fresaclub members.",
-                    calories: 100,
-                    price: 1
-                }
-            ]
-        },
-        {
-            id: 2,
-            name: "Berry Nicee Strawberries",
-            rating: 4.8,
-            categories: [5, 7],
-            priceRating: affordable,
-            photo: images.burger_restaurant_1,
-            duration: "30 - 45 min",
-            image: "https://i.ibb.co/dJRxXmM/0160fedb37f864637a7287b2e818c24a4.png",
-            location: {
-                latitude: 1.5347282806345879,
-                longitude: 110.35632207358996,
-            },
-            menu: [
-                {
-                    menuId: 1,
-                    name: "1 Basket of fresh strawberries.",
-                    photo: images.strawberry_picking,
-                    description: "A Basket of fresh strawberries picked for our fresaclub members.",
-                    calories: 100,
-                    price: 1
-                }
-            ]
-        },
-        {
-            id: 3,
-            name: "Berry Nicee Strawberries",
-            rating: 4.8,
-            categories: [5, 7],
-            priceRating: affordable,
-            photo: images.burger_restaurant_1,
-            duration: "30 - 45 min",
-            image: "https://i.ibb.co/dJRxXmM/0160fedb37f864637a7287b2e818c24a4.png",
-            location: {
-                latitude: 1.5347282806345879,
-                longitude: 110.35632207358996,
-            },
-            menu: [
-                {
-                    menuId: 1,
-                    name: "1 Basket of fresh strawberries.",
-                    photo: images.strawberry_picking,
-                    description: "A Basket of fresh strawberries picked for our fresaclub members.",
-                    calories: 100,
-                    price: 1
-                }
-            ]
-        }
-
-    ]
-
-    // price rating
-    const affordable = 1
-    const fairPrice = 2
-    const expensive = 3
-
-    const restaurantData = [
-        {
-            id: 1,
-            name: "Berry Nicee Strawberries",
-            rating: 4.8,
-            categories: [5, 7],
-            priceRating: affordable,
-            image: "https://i.ibb.co/dJRxXmM/060fedb37f864637a7287b2e818c24a4.png",
-            duration: "30 - 45 min",
             location: {
                 latitude: 1.5347282806345879,
                 longitude: 110.35632207358996,
@@ -244,20 +108,6 @@ const Home = ({ navigation }) => {
     ]
 
     const [categories, setCategories] = React.useState(categoryData)
-    const [orders, setOrders] = React.useState(orderData)
-    const [currentLocation, setCurrentLocation] = React.useState(initialCurrentLocation)
-
-
-
-
-    function getCategoryNameById(id) {
-        let category = categories.filter(a => a.id == id)
-
-        if (category.length > 0)
-            return category[0].name
-
-        return ""
-    }
 
     function renderHeader() {
         return (
@@ -266,8 +116,9 @@ const Home = ({ navigation }) => {
     }
 
     function renderProducts() {
+        getProducts("0x9f3DD64c084C88e8E456e9BAdbc1ebbC624941be");
         return (
-            <MyProductsSlider products={categories}></MyProductsSlider>
+            <MyProductsSlider products={products}></MyProductsSlider>
         )
     }
 
