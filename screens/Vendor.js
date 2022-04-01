f451ba2c193ccbbff1f01a9783c8d8a22923f4e9
Anh Nguyen <anh.nt4288@gmail.com>
2022-04-01 13:46:55 +0700

    add css for vendor

--- screens/Vendor.js
+++ screens/Vendor.js
@@ -1,9 +1,10 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import {
     SafeAreaView,
     View,
     Text,
     StyleSheet,
+    Animated,
     TouchableOpacity,
     Image,
     FlatList,
@@ -13,17 +14,65 @@ import { useWalletConnect } from '@walletconnect/react-native-dapp';
 import { icons, images, SIZES, COLORS, FONTS } from '../constants'
 import AppContext from '../components/AppContext'; 
 import SubNav from "../components/SubNav";
+// import { FloatingLabelInput } from 'react-native-floating-label-input';
 
 
 const Vendor = ({ navigation }) => {
     const connector = useWalletConnect();
     const appContext = useContext(AppContext);
+    const moveText = useRef(new Animated.Value(0)).current;
+
     const [messageVendor, setMessageVendor] = useState("");
     const [storeName, setStoreName] = useState(null);
     const [storeImage, setStoreImage] = useState(null);
     const [storeDescription, setStoreDescription] = useState(null);
     const [storeLat, setStoreLat] = useState(null);
     const [storeLong, setStoreLong] = useState(null);
+
+    useEffect(()=>{
+        readStoreFront();
+    },[connector])
+
+    const onFocusHandler = (value) => {
+        if (value !== "") {
+          moveTextTop();
+        }
+      };
+    
+    const onBlurHandler = (value) => {
+    if (value === "") {
+        moveTextBottom();
+    }
+    };
+
+    const moveTextTop = () => {
+    Animated.timing(moveText, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: true,
+    }).start();
+    };
+
+    const moveTextBottom = () => {
+        Animated.timing(moveText, {
+          toValue: 0,
+          duration: 200,
+          useNativeDriver: true,
+        }).start();
+      };
+
+    const yVal = moveText.interpolate({
+        inputRange: [0, 1],
+        outputRange: [4, -20],
+      });
+    
+      const animStyle = {
+        transform: [
+          {
+            translateY: yVal,
+          },
+        ],
+      };
 
     const readStoreFront = async () => {
         try {    
@@ -87,49 +136,98 @@ const Vendor = ({ navigation }) => {
     function renderStoreFront() {
         return (
             <>
-                <View style={{ width: "100%", height: 30, textAlign: 'center' }}>
+                <View style={{ width: "100%", height: 20, textAlign: 'center' }}>
                     <Text style={{ marginTop: 10, color: "#767070", alignItems: "center" }}>{messageVendor}</Text>
                 </View>
                 <View>
+                <View style={styles.container_input}>
+                    <Animated.View style={[styles.animatedStyle, animStyle]}>
+                        <Text style={styles.label}>Your store name</Text>
+                    </Animated.View>
                     <TextInput
+                        autoCapitalize={"none"}
                         style={styles.input}
-                        onChangeText={setStoreName}
                         value={storeName}
-                        placeholder="Store name"
+                        onChangeText={setStoreName}
+                        editable={true}
+                        onFocus={onFocusHandler(storeName)}
+                        onBlur={onBlurHandler(storeName)}
+                        blurOnSubmit
                     />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setStoreImage}
-                        value={storeImage}
-                        placeholder="Image url"
-                    />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setStoreDescription}
-                        value={storeDescription}
-                        placeholder="Store description"
-                    />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setStoreLat}
-                        value={storeLat}
-                        placeholder="Store latitude"
-                    />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setStoreLong}
-                        value={storeLong}
-                        placeholder="Store longitude"
-                    />
-                    <View style={{alignItems: 'center'}}>
-                        <TouchableOpacity
-                            style={styles.button}
-                            onPress={()=> writeStoreFront()}
-                            >
-                            <Text style={{color: 'white', ...FONTS.h3}}>Create</Text>
-                        </TouchableOpacity>
-                    </View>
                 </View>
+
+                <View style={styles.container_input}>
+                    <Animated.View style={[styles.animatedStyle, animStyle]}>
+                        <Text style={styles.label}>Image Url</Text>
+                    </Animated.View>
+                    <TextInput
+                        autoCapitalize={"none"}
+                        style={styles.input}
+                        value={storeImage}
+                        onChangeText={setStoreImage}
+                        editable={true}
+                        onFocus={onFocusHandler(storeImage)}
+                        onBlur={onBlurHandler(storeImage)}
+                        blurOnSubmit
+                    />
+                </View>
+
+                <View style={styles.container_input}>
+                    <Animated.View style={[styles.animatedStyle, animStyle]}>
+                        <Text style={styles.label}>Store description</Text>
+                    </Animated.View>
+                    <TextInput
+                        autoCapitalize={"none"}
+                        style={styles.input}
+                        value={storeDescription}
+                        onChangeText={setStoreDescription}
+                        editable={true}
+                        onFocus={onFocusHandler(storeDescription)}
+                        onBlur={onBlurHandler(storeDescription)}
+                        blurOnSubmit
+                    />
+                </View>
+
+                <View style={styles.container_input}>
+                    <Animated.View style={[styles.animatedStyle, animStyle]}>
+                        <Text style={styles.label}>Store latitude</Text>
+                    </Animated.View>
+                    <TextInput
+                        autoCapitalize={"none"}
+                        style={styles.input}
+                        value={storeLat}
+                        onChangeText={setStoreLat}
+                        editable={true}
+                        onFocus={onFocusHandler(storeLat)}
+                        onBlur={onBlurHandler(storeLat)}
+                        blurOnSubmit
+                    />
+                </View>
+
+                <View style={styles.container_input}>
+                    <Animated.View style={[styles.animatedStyle, animStyle]}>
+                        <Text style={styles.label}>Store longitude</Text>
+                    </Animated.View>
+                    <TextInput
+                        autoCapitalize={"none"}
+                        style={styles.input}
+                        value={storeLong}
+                        onChangeText={setStoreLong}
+                        editable={true}
+                        onFocus={onFocusHandler(storeLong)}
+                        onBlur={onBlurHandler(storeLong)}
+                        blurOnSubmit
+                    />
+                </View>
+                <View style={{alignItems: 'center'}}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={()=> writeStoreFront()}
+                        >
+                        <Text style={{color: 'white', ...FONTS.h3}}>Create</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
             </>
         )
     }
@@ -157,12 +255,7 @@ const styles = StyleSheet.create({
         shadowRadius: 3,
         elevation: 1,
     },
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-      },
+
     button: {
         alignItems: 'center',
         justifyContent: 'center',
@@ -172,6 +265,39 @@ const styles = StyleSheet.create({
         elevation: 3,
         backgroundColor: COLORS.pink,
         width: 100
+    },
+    container_input: {
+        marginBottom: 10,
+        marginTop: 20,
+        backgroundColor: "#fff",
+        paddingTop: 5,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: "#bdbdbd",
+        borderRadius: 2,
+        width: "90%",
+        alignSelf: "center",
+      },
+    icon: {
+        width: 40,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    input: {
+        fontSize: 13,
+        height: 35,
+        outlineStyle: 'none'
+    },
+    label: {
+        color: "grey",
+        fontSize: 10,
+    },
+    animatedStyle: {
+        top: 5,
+        left: 15,
+        position: 'absolute',
+        borderRadius: 90,
+        zIndex: 10000,
     },
 })
 
