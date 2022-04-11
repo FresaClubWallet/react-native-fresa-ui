8da5085f57d374fc52b6496de72e12877f65d5df
Anh Nguyen <anh.nt4288@gmail.com>
2022-04-12 01:21:38 +0700

    add babel

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,8 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // plugins: [
-    //   'react-native-web'
-    // ],
+    plugins: [
+      'react-native-web'
+    ],
   };
 };
